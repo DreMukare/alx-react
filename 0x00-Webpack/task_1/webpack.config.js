19aec80076c97417bb5f0fd3c2e6873134b8255c
Andrew Mukare <dremukare@gmail.com>
2021-12-20 16:28:27 +0000

    feat: done task 1

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		main: path.resolve(__dirname, './js/dashboard_main.js'),
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'public'),
+	},
+};
