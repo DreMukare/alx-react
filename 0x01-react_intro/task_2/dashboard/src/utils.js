4e22fd396c79a17dd05b39fdad457259d78e9620
Andrew Mukare <dremukare@gmail.com>
2021-12-21 11:52:00 +0000

    feat: done task 3

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -2,3 +2,6 @@ export const getFullYear = () => new Date().getFullYear();
 
 export const getFooterCopy = (isIndex) =>
 	isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+
+export const getLatestNotification = () =>
+	'<strong>Urgent Requirement</strong> - complete by EOD';
