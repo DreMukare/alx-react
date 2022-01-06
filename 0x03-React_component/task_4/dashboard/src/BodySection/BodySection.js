054449c093f5374bfde57ab6d336dea8bdf1eb66
Andrew Mukare <dremukare@gmail.com>
2022-01-06 16:15:29 +0000

    feat: done task 7

---
+++ 0x03-React_component/task_4/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BodySection = ({ title, children }) => {
+	return (
+		<div className='bodySection'>
+			<h2>{title}</h2>
+			{children}
+		</div>
+	);
+};
+
+BodySection.propTypes = {
+	title: PropTypes.string,
+	children: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+	]).isRequired,
+};
+
+export default BodySection;
---
+++ 0x03-React_component/task_4/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import BodySection from './BodySection';
+import './BodySectionWithMarginBottom.css';
+import PropTypes from 'prop-types';
+
+const BodySectionWithMarginBottom = ({ title, children }) => {
+	return (
+		<div className='bodySectionWithMargin'>
+			<BodySection title={title}>{children}</BodySection>
+		</div>
+	);
+};
+
+BodySectionWithMarginBottom.propTypes = {
+	title: PropTypes.string,
+	children: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+	]).isRequired,
+};
+
+export default BodySectionWithMarginBottom;
