9a446c6011d21443f636eafa7a27d31a5e77f598
Andrew Mukare <dremukare@gmail.com>
2022-01-20 18:16:45 +0000

    feat: modified getImmutableObject

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,5 @@
-import { fromJS } from 'immutable';
+import { Map } from 'immutable';
 
-const getImmutableObject = (object) => fromJS(object);
+const getImmutableObject = (object) => Map(object);
 
 module.exports = getImmutableObject;
