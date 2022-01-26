c19aed55c52404eee1131ae3860900943127c9e2
Andrew Mukare <dremukare@gmail.com>
2022-01-26 18:00:18 +0000

    feat: used normalizr to normalize JSON data for easier consumption

---
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -0,0 +1,20 @@
+import notificationData from '../../../../notifications.json';
+import { normalize, schema } from 'normalizr';
+
+const user = new schema.Entity('users');
+const message = new schema.Entity('messages', {}, { idAttribute: 'guid' });
+const notification = new schema.Entity('notification', {
+	author: user,
+	context: message,
+});
+
+const normalized = normalize(notificationData, [notification]);
+
+export default function getAllNotificationsByUser(userId) {
+	// console.log(normalized);
+	return notificationData
+		.filter((notification) => notification.author.id === userId)
+		.map((notification) => notification.context);
+}
+
+export { normalized };
