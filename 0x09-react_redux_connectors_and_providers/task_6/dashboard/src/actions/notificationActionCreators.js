92ddd3d7b7d523069b409c01e9a06387057c240a
Andrew Mukare <dremukare@gmail.com>
2022-02-08 18:56:24 +0000

    feat: updated notification selector

--- 0x09-react_redux_connectors_and_providers/task_6/dashboard/src/actions/notificationActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_6/dashboard/src/actions/notificationActionCreators.js
@@ -1,19 +1,20 @@
 import {
-	FETCH_NOTIFICATIONS_SUCCESS,
 	MARK_AS_READ,
 	SET_TYPE_FILTER,
 	SET_LOADING_STATE,
+	FETCH_NOTIFICATIONS_SUCCESS,
 } from './notificationActionTypes';
+
 import 'node-fetch';
 
-export const markAsRead = (index) => {
+export const markAsAread = (index) => {
 	return {
 		type: MARK_AS_READ,
 		index,
 	};
 };
 
-export const boundMarkAsRead = (index) => dispatch(markAsAread(index));
+export const boundMarkAsAread = (index) => dispatch(markAsAread(index));
 
 export const setNotificationFilter = (filter) => {
 	return {
@@ -22,26 +23,30 @@ export const setNotificationFilter = (filter) => {
 	};
 };
 
-export const setLoadingState = (loading) => ({
-	type: SET_LOADING_STATE,
-	loading,
-});
+export const boundSetNotificationFilter = (filter) =>
+	dispatch(setNotificationFilter(filter));
 
-export const setNotifications = (notifications) => ({
-	type: FETCH_NOTIFICATIONS_SUCCESS,
-	notifications,
-});
+export const setLoadingState = (loading) => {
+	return {
+		type: SET_LOADING_STATE,
+		loading,
+	};
+};
+
+export const setNotifications = (data) => {
+	return {
+		type: FETCH_NOTIFICATIONS_SUCCESS,
+		data,
+	};
+};
 
 export const fetchNotifications = () => {
 	return (dispatch) => {
 		dispatch(setLoadingState(true));
-		return fetch('../../notifications.json')
+		return fetch('./notifications.json')
 			.then((res) => res.json())
 			.then((data) => dispatch(setNotifications(data)))
 			.catch((error) => {})
-			.finally(dispatch(setLoadingState(false)));
+			.finally(() => dispatch(setLoadingState(false)));
 	};
 };
-
-export const boundSetNotificationFilter = (filter) =>
-	dispatch(setNotificationFilter(filter));
